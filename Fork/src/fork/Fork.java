3984242a54891da8ed511d0fded859323fcf73b5
AlejndroSG <alex2005sg@gmail.com>
2024-05-21 13:58:32 +0200

    V1

--- Fork/src/fork/Fork.java
+++ Fork/src/fork/Fork.java
@@ -15,7 +15,7 @@ public class Fork {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola soy Alex y te he modificado el código");
     }
     
 }
